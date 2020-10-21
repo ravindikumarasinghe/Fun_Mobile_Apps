8ecdb03e54ec4a01f75965420a731b3b651b2d8b
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-10-22 00:01:24 +0530

    pp

--- tipcalculatorv0/app/src/main/java/com/example/tipcalculatorv0/MainActivity.java
+++ tipcalculatorv0/app/src/main/java/com/example/tipcalculatorv0/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    hduwhiidwud
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+}55h5h5h5h5h
+rvrv
