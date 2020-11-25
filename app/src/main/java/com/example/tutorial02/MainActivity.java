7fc9c95120226494135634e5bf5d73d637c3a4e9
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-11-25 23:30:26 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/tutorial02/MainActivity.java
+++ app/src/main/java/com/example/tutorial02/MainActivity.java
@@ -4,11 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.util.Log;
+
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "champcode";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG,"onCreate");
+    }
+
+    @Override
+
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"onStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG,"onRestart");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,"onPause");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG,"onStop");
     }
 }
